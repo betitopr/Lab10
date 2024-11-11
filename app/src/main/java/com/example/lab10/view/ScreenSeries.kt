d69b4ff3be3257c476789452bdd0518e53d2bfa7
betitopr <betomood11@gmail.com>
2024-11-10 20:35:13 -0500

    Agregar vistas para crud en las series

--- app/src/main/java/com/example/lab10/view/ScreenSeries.kt
+++ app/src/main/java/com/example/lab10/view/ScreenSeries.kt
@@ -1,4 +1,5 @@
-package com.example.lab10.views
+package com.example.lab10.view
+
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -28,8 +29,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
-
+import androidx.navigation.NavHostController
+import com.tecsup.lab10.data.SerieApiService
+import com.tecsup.lab10.data.SerieModel
+import kotlinx.coroutines.delay
 
 @Composable
 fun ContenidoSeriesListado(navController: NavHostController, servicio: SerieApiService) {
@@ -97,3 +100,118 @@ fun ContenidoSeriesListado(navController: NavHostController, servicio: SerieApiS
         }
     }
 }
+@Composable
+fun ContenidoSerieEditar(navController: NavHostController, servicio: SerieApiService, pid: Int = 0 ) {
+    var id by remember { mutableStateOf<Int>(pid) }
+    var name by remember { mutableStateOf<String?>("") }
+    var release_date by remember { mutableStateOf<String?>("") }
+    var rating by remember { mutableStateOf<String?>("") }
+    var category by remember { mutableStateOf<String?>("") }
+    var grabar by remember { mutableStateOf(false) }
+
+    if (id != 0) {
+        LaunchedEffect(Unit) {
+            val objSerie = servicio.selectSerie(id.toString())
+            delay(100)
+            name = objSerie.body()?.name
+            release_date = objSerie.body()?.release_date
+            rating = objSerie.body()?.rating.toString()
+            category = objSerie.body()?.category
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        // Spacer(Modifier.height(50.dp))
+        TextField(
+            value = id.toString(),
+            onValueChange = { },
+            label = { Text("ID (solo lectura)") },
+            readOnly = true,
+            singleLine = true
+        )
+        TextField(
+            value = name!!,
+            onValueChange = { name = it },
+            label = { Text("Name: ") },
+            singleLine = true
+        )
+        TextField(
+            value = release_date!!,
+            onValueChange = { release_date = it },
+            label = { Text("Release Date:") },
+            singleLine = true
+        )
+        TextField(
+            value = rating!!,
+            onValueChange = { rating = it },
+            label = { Text("Rating:") },
+            singleLine = true
+        )
+        TextField(
+            value = category!!,
+            onValueChange = { category = it },
+            label = { Text("Category:") },
+            singleLine = true
+        )
+        Button(
+            onClick = {
+                grabar = true
+            }
+        ) {
+            Text("Grabar", fontSize=16.sp)
+        }
+    }
+
+    if (grabar) {
+        val objSerie = SerieModel(id,name!!, release_date!!, rating!!.toInt(), category!!)
+        LaunchedEffect(Unit) {
+            if (id == 0)
+                servicio.insertSerie(objSerie)
+            else
+                servicio.updateSerie(id.toString(), objSerie)
+        }
+        grabar = false
+        navController.navigate("series")
+    }
+}
+@Composable
+fun ContenidoSerieEliminar(navController: NavHostController, servicio: SerieApiService, id: Int) {
+    var showDialog by remember { mutableStateOf(true) }
+    var borrar by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = "Confirmar Eliminación") },
+            text = {  Text("¿Está seguro de eliminar la Serie?") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        showDialog = false
+                        borrar = true
+                    } ) {
+                    Text("Aceptar")
+                }
+            },
+            dismissButton = {
+                Button( onClick = { showDialog = false } ) {
+                    Text("Cancelar")
+                    navController.navigate("series")
+                }
+            }
+        )
+    }
+    if (borrar) {
+        LaunchedEffect(Unit) {
+            // val objSerie = servicio.selectSerie(id.toString())
+            servicio.deleteSerie(id.toString())
+            borrar = false
+            navController.navigate("series")
+        }
+    }
+}
+
